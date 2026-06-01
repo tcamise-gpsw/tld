246c558707ecbcdfed8e8ac2ea6db0ded9dc0812
Mertcikla <10576032+Mertcikla@users.noreply.github.com>
2026-06-01 13:42:59 +0000

    chore: bump version to v2.2.8 [skip ci]

--- cmd/version/version.go
+++ cmd/version/version.go
@@ -14,7 +14,7 @@ import (
 
 // Version is the current version of the CLI.
 // This is overridden by ldflags during build.
-var Version = "2.2.7"
+var Version = "2.2.8"
 
 func NewVersionCmd() *cobra.Command {
 	c := &cobra.Command{
